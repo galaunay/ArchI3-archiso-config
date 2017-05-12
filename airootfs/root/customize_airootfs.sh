92b6af170fbf7a94fcd3be4def6f8822df2345b8
muahah <gnieheh@gmail.com>
2017-05-12 08:52:27 +0200

    Add some additional packages

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -9,18 +9,24 @@ locale-gen
 
 # localtime
 ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime 
-# # configure root
-# usermod -s /usr/bin/zsh root
+
+# configure root
+usermod -s /usr/bin/zsh root
 # chmod 700 /root
+
+# Keyboard
+loadkeys fr
 
 # GIve guest adminrights
 groupadd sudo
 sed -i 's/# \(%sudo\tALL=(ALL) ALL\)/\1/' /etc/sudoers
-# create user
+
+# Create user
 useradd -m -p "" -g users -G "sudo,adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /usr/bin/zsh guest
 cp -aT /etc/skel/ /home/guest
 chown guest:users /home/guest -R
-# update config through git if possiblr
+
+# Update user config through git if possible
 bck_dir=$(pwd)
 cd /home/guest
 git remote update
@@ -28,6 +34,9 @@ git checkout master
 git pull --rebase
 git submodule update --init --recursive
 cd $bck_dir
+
+# Emacs
+emacs --eval '(server-shutdown)'
 
 # SSHD
 sed -i 's/#\(PermitRootLogin \).\+/\1no/' /etc/ssh/sshd_config
@@ -39,28 +48,26 @@ sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
 # Journal
 sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
 
-# shutdown keys
+# Shutdown keys
 sed -i 's/#\(HandleSuspendKey=\)suspend/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
 
 # Additional packages
 #   Calamares
+git clone https://github.com/calamares/calamares /tmp/calamares
 bck_dir=$(pwd)
-cp -r /root/additional_packages/calamares /tmp/calamares
 cd /tmp/calamares
 git submodule update --init --recursive
-git clean -f -d
-rm -fr build
 mkdir -p build; cd build
-cmake ..; make
-rm -rf /root/additional_packages/calamares
+cmake ..; make; make install
 cd $bck_dir
+
+#   Packer
+git clone https://aur.archlinux.org/packer.git /tmp/packer
+makepkg -is --needed --noconfirm /tmp/packer
 
 # Services
 systemctl enable pacman-init.service choose-mirror.service
 systemctl set-default multi-user.target
 
-# # Packer
-# git clone https://aur.archlinux.org/packer.git /tmp/packer
-# makepkg -is /tmp/packer
