9b802fccf7ccc749a55398ca89c38dd42a1533d9
muahah <gnieheh@gmail.com>
2017-06-17 02:48:02 +0200

    Use external repo

--- local_repo/update_repo_database.sh
+++
@@ -1,8 +0,0 @@
-# build repo
-repo-add repo.db.tar.gz pkg/*
-sudo mkdir -p /etc/pacman.d/repo
-sudo cp repo.* /etc/pacman.d/repo/
-# sudo cp pkg/* /etc/pacman.d/repo/
-sudo mkdir -p ../airootfs/etc/pacman.d/repo
-sudo cp repo.* ../airootfs/etc/pacman.d/repo/
-# sudo cp pkg/* ../airootfs/etc/pacman.d/repo/
